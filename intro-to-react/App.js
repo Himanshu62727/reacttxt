dcd17137315d55638584fe0a806bdfb813b49199
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2024-11-22 11:39:54 +0530

    Create App.js

---
+++ intro-to-react/App.js
@@ -0,0 +1,26 @@
+// import Button from "./button"
+// function Hii(){
+    
+//     let age = 50
+//     let headingStyle = {color: "red", background:"Wheat"}
+
+//     return(
+//         <div>
+//             <h1 style={headingStyle}>Heloooo</h1>
+//             <h3>Age is {age}</h3>
+//             <h4> {10+12} </h4>
+//             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi molestiae aliquam sint quaerat ab, ex veniam, perspiciatis vitae rerum dolores quam maiores tempore deleniti dolor eum libero, laborum reprehenderit adipisci!</p>
+//         </div>
+//     )
+// }
+
+// App.js
+import React from 'react';
+
+function Hii() {
+  return <h1>Hello, World!
+    <p>this is Himanshu</p>
+  </h1>;
+}
+
+export default Hii;
